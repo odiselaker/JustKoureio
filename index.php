5a024d38d6e5e463a0862f00e58aa520837d3881
Odisseas Papanidis <odisseaspapanidis4@gmail.com>
2026-04-12 21:39:44 +0300

    evala tis photo

--- index.php
+++ index.php
@@ -1,9 +1,30 @@
+<link rel="stylesheet" href="/my-project/CSS/style.css">
+<link rel="stylesheet" href="/my-project/CSS/index.css">
+
 <?php $page_title = "Home"; ?>
 
 <?php require_once "db/config.php"; ?>
 
 <?php include_once 'includes/header.php'; ?>
 
+<div class="container">
+    <div class="photos d-flex">
+        <div class="bigphoto">
+            <img src="/my-project/assets/images/photoBIG.jpg"
+                height="auto" 
+                width="800px"
+                alt="barber">
+        </div>
 
+        <div class="smallphoto">
+            <img src="/my-project/assets/images/photoLITTLE.jpg"
+                height="auto" 
+                width="800px"
+                alt="barber">
+
+            <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque officia recusandae repellendus ut consequuntur laboriosam aperiam labore deleniti nesciunt. Vel quibusdam consequatur, asperiores ipsam neque sit iure rem architecto fuga?</p>
+        </div>
+    </div>
+</div>
 
 <?php include_once 'includes/footer.php'; ?>
--- insert-about.php
+++ insert-about.php
@@ -1,5 +1,46 @@
 <?php require_once "db/config.php"; ?>
 
+<style>
+    body {
+        background-color: dimgray;
+    }
+
+    .inserts {
+        background-color: grey;
+        border-radius: 10px;
+        border-radius: 15px;
+        width: 35%;
+        height: auto;
+        margin: 0 auto; 
+        margin-top: 25px;
+        text-align: center;
+        margin-top: 15%;
+        padding: 40px;
+    }
+
+    .inserts input{
+        border-radius: 5px;
+        background-color: black;
+        color: gold;
+    }
+
+    .inserts textarea{
+        border-radius: 5px;
+        background-color: black;
+        color: gold;
+        width: 300px;
+        height: 300px;
+    }
+
+    .inserts button{
+        border-radius: 5px;
+        background-color: gold;
+        color: black;
+    }
+
+
+</style>
+
 <?php 
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -14,8 +55,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 }
 ?>
 
-<form method="POST">
-    <input type="text" name="title" placeholder="Title"><br>
-    <textarea name="content" placeholder="Content"></textarea><br>
-    <button type="submit">Save</button>
-</form>
+<div class="inserts">
+    <form method="POST">
+      <input type="text" name="title" placeholder="Title"><br><br>
+      <textarea name="content" placeholder="Content"></textarea><br><br>
+      <button type="submit">Save</button>
+    </form>
+</div>
